4a08db812da86b42d47fb887311dfe45c4dcfda9
Himadri Majumdar <himadri.majumdar@ericsson.com>
2016-08-08 11:28:57 +0530

    users.js changed

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var users = ['Ram','Srinivas','Sudhakar','Pankaj'];
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('respond with a resource');
+  res.json(users);
 });
 
 module.exports = router;
